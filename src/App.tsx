6ba961d826edc771e1d138f955ae65a5ca2063c8
mikeybkats <michaelbarakat@BCSR-10960.guest.corp.microsoft.com>
2019-05-14 11:02:06 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,156 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IReservationState {
+  isGoing: boolean;
+  numberOfGuests: number;
 }
 
-export default App;
+interface IReservationTouchState {
+  numberOfGuests: boolean;
+  isGoing: boolean;
+}
+
+interface IMiniFormikProps {
+  children: (state: IMiniFormikState) => JSX.Element;
+  onSubmit: (values: IReservationState) => void;
+  initialValues: IReservationState;
+  initialTouched: IReservationTouchState;
+}
+
+interface IMiniFormikState {
+  values: IReservationState;
+  touched: IReservationTouchState;
+  errors: {};
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (event: React.ChangeEvent<HTMLFormElement>) => void;
+}
+
+class MiniFormik extends React.Component<IMiniFormikProps, IMiniFormikState> {
+  constructor(props: IMiniFormikProps){
+    super(props);
+    this.state = {
+      values: this.props.initialValues || {},
+      touched: this.props.initialTouched || {},
+      errors: {},
+      handleSubmit: () => {}
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+ private handleBlur(event: React.ChangeEvent<HTMLInputElement>) {
+    const target = event.currentTarget;
+    const name = target.name;
+    event.persist();
+
+    this.setState(prevState => ({
+      touched: {
+        ...prevState.touched,
+        [name]: true
+      }
+    }))
+  }
+
+  private handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const target = event.currentTarget;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    event.persist();
+    
+    this.setState(prevState => ({
+      values: {
+        ...prevState.values,
+        [name]: value
+      }
+    }))
+  }
+
+  private handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.values)
+  }
+
+  public render(): JSX.Element {
+    return <React.Fragment>{
+      this.props.children({...this.state, 
+        handleChange: this.handleInputChange, 
+        handleBlur: this.handleBlur, 
+        handleSubmit: this.handleSubmit
+      })}</React.Fragment>
+  }
+}
+
+class Reservation extends React.Component<{}, IReservationState> {
+  private alertValues = (values: IReservationState) => {
+    alert(JSON.stringify(values, null, 2))
+  }
+
+  public render(): JSX.Element {
+    return (
+      <MiniFormik 
+        initialValues={{
+          isGoing: true,
+          numberOfGuests: 2
+        }}
+        initialTouched={{numberOfGuests: false, isGoing: false }}
+        onSubmit={values => this.alertValues(values)}
+       >
+      {props => { 
+       /**
+          this is called a render prop
+          it's a method for sharing code between react components.
+          https://reactjs.org/docs/render-props.html
+        */
+        const { values, errors, touched, handleBlur, handleChange, handleSubmit } = props;
+        return (
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          alignContent: "center",
+          justifyContent: "center",
+          padding: "50px"
+        }}>
+        <form style={{
+          width: "300px",
+          height: "200px",
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+          border: "1px blue solid",
+          padding: "40px 20px"
+        }}
+          onSubmit={handleSubmit}
+        >
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={values.isGoing}
+            onChange={handleChange} 
+            onBlur={handleBlur}
+            />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={values.numberOfGuests}
+            onChange={handleChange} 
+            onBlur={handleBlur}
+            />
+        </label>
+        <pre>{JSON.stringify(props, null, 2)}</pre>
+        </form></div>
+      )}}
+      </MiniFormik>
+    );
+  }
+}
+
+export default Reservation;
